471729a6006a402e110475e2708418e791f01f59
Glitch (same-value) <none>
2017-10-31 04:47:21 +0000

    Checkpoint

--- server.js
+++ server.js
@@ -4,23 +4,26 @@ var mongodb = require("mongodb")
 var MongoClient = mongodb.MongoClient
 var url = 'mongodb://jopet:jopet@ds237445.mlab.com:37445/url-shortener-microservice-db'
 
-app.get("/:qwe", function(req, res){
+app.get("/", function(req, res){
+  //var path
   MongoClient.connect(url, function(err, db){
     if (err){
       console.log("did not connect to " + url)
     }
     if (db) {
-      var cunt
+      /*
       db.collection("urls").insertOne({
         url: req.params.qwe,
         shortened: req.params.qwe
       })
-      db.collection("urls", function(err, collection){
-        cunt = collection.count()
+      */
+      db.collection("urls").find({}).toArray(function(err, results) {
+        path = results
+        res.end(path.length)
       })
-      res.end(cunt)
     }
   })
+  
 })
 
 app.listen(process.env.PORT)
