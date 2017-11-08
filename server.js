0f333694d2bea5876ddfd72567096f27866c025d
Glitch (same-value) <none>
2017-11-08 18:01:00 +0000

    Checkpoint

--- server.js
+++ server.js
@@ -10,12 +10,12 @@ app.get("/", function(req,res){
   res.send("welcome to my 3rd fcc backend webdev certification basejump: url shortener microservice. </br> go to https://same-value.glitch.me/new/*your-url* to try the app")
 })
 
-app.get("/*", function(req, res){
-  var num = req.url.replace('/', '')
+app.get("/:shortened", function(req, res){
+  var num = req.params.shortened.replace('/', '')
   if(!isNaN(num)){
     MongoClient.connect(url, function(err, db){
       if (db){
-        db.collection("urls").find({short_url: Number(num)}, {_id: 0, original_url: 1, short_url: 1}).toArray(function(err, doc){
+        db.collection("urls").find({short_url: 'https://same-value.glitch.me/' + Number(num)}, {_id: 0, original_url: 1, short_url: 1}).toArray(function(err, doc){
           if (err) {
             res.send("err")
           } else {
@@ -32,8 +32,52 @@ app.get("/*", function(req, res){
       }
     })
   }
-  
 })
 
+
+app.get("/new/*", function(req, res){
+//var path
+  MongoClient.connect(url, function(err, db){
+    if (err){
+      res.end("did not connect to " + url)
+    }
+    if (db) {
+      var longUrl = req.url.replace('/new/', '')
+      if(isNaN(longUrl)){
+      //res.end("connected to " + url)
+        if (validUrl.isUri(longUrl)){
+              longUrl = encodeURI(longUrl)
+
+              var newUrl = {
+                original_url: "",
+                short_url: ""
+              }
+
+              db.collection("urls").count(function (err, count){
+                 newUrl = {
+                  original_url: longUrl,
+                  short_url: 'https://same-value.glitch.me/' + Number(count)
+                }
+
+                db.collection("urls").find({original_url: longUrl}, {_id: 0, original_url: 1, short_url: 1}).toArray(function(err, doc){
+                  if (doc[0]){
+                    res.send(JSON.stringify(doc))
+                  } else {
+                    db.collection("urls").insertOne(newUrl)
+                    db.collection("urls").find({original_url: longUrl}, {_id: 0, original_url: 1, short_url: 1}).toArray(function(err, doc){
+                      res.send(JSON.stringify(doc))
+                    }
+                  }
+                })  
+              })
+        } else {
+              res.send("invalid url")
+        }
+      } else {
+          res.send("invalid url")
+      }
+    }
+  })
+ })
 
 app.listen(process.env.PORT)
