8075399afcb086492132faa2b985a3010b252899
Glitch (same-value) <none>
2017-11-08 04:26:23 +0000

    Checkpoint

--- server.js
+++ server.js
@@ -12,30 +12,51 @@ app.get("/:qwe", function(req, res){
     }
     if (db) {
       //res.end("connected to " + url)
+      function ValidURL(str) {
+        var pattern = new RegExp('^(https?:\/\/)?'+ // protocol
+          '((([a-z\d]([a-z\d-]*[a-z\d])*)\.)+[a-z]{2,}|'+ // domain name
+          '((\d{1,3}\.){3}\d{1,3}))'+ // OR ip (v4) address
+          '(\:\d+)?(\/[-a-z\d%_.~+]*)*'+ // port and path
+          '(\?[;&a-z\d%_.~+=-]*)?'+ // query string
+          '(\#[-a-z\d_]*)?$','i'); // fragment locater
+        if(!pattern.test(str)) {
+          return false;
+        } else {
+          return true;
+        }
+      }
       
-      var longUrl = req.params.qwe
-      
-      var newUrl = {
+      if (ValidURL(req.params.qwe)) { 
+        
+        var longUrl = req.params.qwe
+        
+        var newUrl = {
           url: "",
           shortened: ""
-      }
-      /*
-      
-      db.collection("urls").find({url: req.params.qwe}).toArray(function(err, doc){
-        
-        
-      })
-      */
-      db.collection("urls").count(function (err, count){
-         newUrl = {
-          url: longUrl,
-          shortened: Number(count)
         }
-        
-        db.collection("urls").insertOne(newUrl)
+        /*
 
-        res.send(JSON.stringify(newUrl))
-      })
+        db.collection("urls").find({url: req.params.qwe}).toArray(function(err, doc){
+
+
+        })
+        */
+        db.collection("urls").count(function (err, count){
+           newUrl = {
+            url: longUrl,
+            shortened: Number(count)
+          }
+
+          db.collection("urls").insertOne(newUrl)
+
+          res.send(JSON.stringify(newUrl))
+        })
+
+      } else {
+        res.send("invalid url")
+      }
+      
+     
       
     }
   })
